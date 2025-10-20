8358cef91f138b0087f5a5855e8341b7078e873e
Grigoriy <quracau@gmail.com>
2025-10-20 23:09:30 +0300

    feat(security): Implement base User and Role entities, enum, and repositories

---
+++ src/main/java/com/study/FlowTrack/model/Role.java
@@ -0,0 +1,29 @@
+package com.study.FlowTrack.model;
+
+import com.study.FlowTrack.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id",updatable = false,nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private UserRole name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+}
---
+++ src/main/java/com/study/FlowTrack/model/User.java
@@ -0,0 +1,30 @@
+package com.study.FlowTrack.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
