5c830289bd1700903972c7081f9f22213226e795
Grigoriy <quracau@gmail.com>
2025-10-22 21:30:55 +0300

    feat(exceptions): Introduce InitialRoleNotFoundException for configuration errors

---
+++ src/main/java/com/study/FlowTrack/exception/InitialRoleNotFoundException.java
@@ -0,0 +1,7 @@
+package com.study.FlowTrack.exception;
+
+public class InitialRoleNotFoundException extends ResourceNotFoundException {
+    public InitialRoleNotFoundException(String message) {
+        super(message);
+    }
+}
