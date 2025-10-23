a42b4f0fc7e2ad6ca950edadb4b7e6bdca60b6df
Grigoriy <quracau@gmail.com>
2025-10-23 10:53:31 +0300

    feat(startup): Add StatusTask initialization to data seeder

--- src/main/java/com/study/FlowTrack/config/RoleDataInitializer.java
+++ src/main/java/com/study/FlowTrack/config/RoleDataInitializer.java
@@ -1,7 +1,10 @@
 package com.study.FlowTrack.config;
 
+import com.study.FlowTrack.enums.StatusTask;
 import com.study.FlowTrack.enums.SystemRole;
+import com.study.FlowTrack.model.StatusTaskEntity;
 import com.study.FlowTrack.model.SystemRoleEntity;
+import com.study.FlowTrack.repository.StatusTaskEntityRepository;
 import com.study.FlowTrack.repository.SystemRoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,18 +15,27 @@ import java.util.stream.Collectors;
 @Component
 public class RoleDataInitializer implements CommandLineRunner {
     private final SystemRoleRepository systemRoleRepository;
+    private final StatusTaskEntityRepository statusTaskEntityRepository;
 
-    public RoleDataInitializer(SystemRoleRepository systemRoleRepository){
+    public RoleDataInitializer(SystemRoleRepository systemRoleRepository, StatusTaskEntityRepository statusTaskEntityRepository){
         this.systemRoleRepository = systemRoleRepository;
+        this.statusTaskEntityRepository = statusTaskEntityRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
         systemRoleRepository.saveAll(Arrays.stream(SystemRole.values())
-                .filter(role -> !systemRoleRepository.existsByName(role))
+                .filter(role -> !systemRoleRepository.existsBySystemRoleName(role))
                 .map(role -> new SystemRoleEntity(role))
                 .collect(Collectors.toSet()));
+
+        statusTaskEntityRepository.saveAll(Arrays.stream(StatusTask.values())
+                .filter(statusTask -> !statusTaskEntityRepository.existsByStatusTask(statusTask))
+                .map(statusTask -> new StatusTaskEntity(statusTask))
+                .collect(Collectors.toSet()));
+
+
+
     }
 
 }
