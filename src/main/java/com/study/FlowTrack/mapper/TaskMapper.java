d5e211007bea6f00b0b835163f41b297ac7544fa
Grigoriy <quracau@gmail.com>
2025-11-02 14:06:42 +0300

    refactor(task): Standardize Task DTOs and update Mapper for public identifiers

--- src/main/java/com/study/FlowTrack/mapper/TaskMapper.java
+++ src/main/java/com/study/FlowTrack/mapper/TaskMapper.java
@@ -4,6 +4,7 @@ import com.study.FlowTrack.model.Task;
 import com.study.FlowTrack.payload.task.TaskCreationDto;
 import com.study.FlowTrack.payload.task.TaskResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,6 +14,9 @@ import java.util.List;
 public interface TaskMapper {
     Task toEntity(TaskCreationDto dto);
 
+    @Mapping(source = "project.key", target = "projectKey")
+    @Mapping(source = "taskNumber", target = "taskNumber")
+    @Mapping(source = "statusTaskEntity.statusTask", target = "statusTask")
     TaskResponseDto toResponseDto(Task task);
 
     List<TaskResponseDto> toResponseListDto(List<Task> tasks);
--- src/main/java/com/study/FlowTrack/payload/task/TaskResponseDto.java
+++ src/main/java/com/study/FlowTrack/payload/task/TaskResponseDto.java
@@ -1,10 +1,16 @@
 package com.study.FlowTrack.payload.task;
 
 import com.study.FlowTrack.enums.StatusTask;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class TaskResponseDto {
     private Long id;
     private String title;
     private String description;
     private StatusTask statusTask;
+    private String projectKey;
+    private Long taskNumber;
 }
