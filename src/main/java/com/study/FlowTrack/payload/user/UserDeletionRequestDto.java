d5abf46929cd1ab3edb3dda7e4c9e6eca90e3acb
Grigoriy <quracau@gmail.com>
2025-11-03 22:32:09 +0300

    feat: add DTO validation to UserDTO's

--- src/main/java/com/study/FlowTrack/payload/user/UserDeletionRequestDto.java
+++ src/main/java/com/study/FlowTrack/payload/user/UserDeletionRequestDto.java
@@ -1,10 +1,12 @@
 package com.study.FlowTrack.payload.user;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class UserDeletionRequestDto {
+    @NotNull(message = "User ID to delete must be provided.")
     private Long userIdToDelete;
 }
--- src/main/java/com/study/FlowTrack/payload/user/UserRoleUpdateRequestDto.java
+++ src/main/java/com/study/FlowTrack/payload/user/UserRoleUpdateRequestDto.java
@@ -1,12 +1,16 @@
 package com.study.FlowTrack.payload.user;
 
 import com.study.FlowTrack.enums.ProjectRole;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class UserRoleUpdateRequestDto {
+    @NotNull(message = "User ID to update must be provided.")
     private Long userIdToUpdate;
+
+    @NotNull(message = "New role must be specified.")
     private ProjectRole role;
 }
