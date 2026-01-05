d7b7007ea025ed892c22b91c7d821535607aa488
Grigoriy <quracau@gmail.com>
2026-01-05 22:31:42 +0300

    test: cleanup default test configurations

--- src/test/java/com/study/FlowTrack/FlowTrackApplicationTests.java
+++ src/test/java/com/study/FlowTrack/FlowTrackApplicationTests.java
@@ -1,15 +1,8 @@
 package com.study.FlowTrack;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class FlowTrackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+public class FlowTrackApplicationTests {
 
 }
